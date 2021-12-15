d52312e14a2309617d540a83d6f291b2986c0567
Darby Benfield <thinxing@hotmail.com>
2021-12-14 21:43:17 -0700

    exercise 4 complete

--- exercise-4.py
+++ exercise-4.py
@@ -12,3 +12,15 @@
 #      isosceles - two sides are the same length
 # 3. Print a message such as:
 #      - A triangle with sides of <a>, <b> & <c> is a <type of triangle> triangle
+
+input("enter the lengths of three sides of a triangle")
+a = input ("a: ")
+b = input ("b: ")
+c = input ("c: ")
+if a == b and b == c:
+  print(f"This is an equalateral triangle - all three sides, {a}, {b}, {c}, are equal in length")
+elif a !=b and a !=c and b !=c:
+  print(f"This is a scalene triangle - all three sides, {a}, {b}, {c}, are not equal in length ")
+elif a == b or a == c or b == c:
+  print(f"This is an isosceles triangle. Two sides out of {a}, {b}, and {c}, match in length. ")
+  
